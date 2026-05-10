f1346a414e0b2f67d18c3a69eddc4e8261044522
Ajay Malwal <106762695+Ajaymalwal@users.noreply.github.com>
2026-05-10 10:55:00 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    private Map<Integer,List<Integer>> map = new HashMap<>();
+    private Set<Integer> visit = new HashSet<>();
+    public int countComponents(int n, int[][] edges) {
+        if(n == 1) return 1;
+        for(int i=0; i<n; i++){
+            map.put(i,new ArrayList<>());
+        }
+        for(int[] edge : edges){
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        int components = 0;
+
+        for(int i=0; i<n; i++){
+            if(!visit.contains(i)) components++;
+            dfs(i);
+            
+        }
+        return components;
+    }
+
+    private void dfs(int node){
+        if(visit.contains(node)) return;
+        visit.add(node);
+
+        for(int i : map.get(node)){
+            dfs(i);
+        }
+        return;
+    }
+}
